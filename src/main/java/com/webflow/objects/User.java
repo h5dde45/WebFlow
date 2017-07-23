30fcb50dd74b4218c561ef7e3acbc5a00b616604
tmvf <tmvf@yandex.ru>
2017-07-23 12:14:40 +0300

    scope

--- src/main/java/com/webflow/objects/User.java
+++ src/main/java/com/webflow/objects/User.java
@@ -4,11 +4,16 @@ import java.io.Serializable;
 
 public class User implements Serializable{
 
-//    private static final long sessionVersionUID=
-
-
     private String name;
     private String password;
+
+    public User() {
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
 
     public String getName() {
         return name;
@@ -24,5 +29,23 @@ public class User implements Serializable{
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        return password != null ? password.equals(user.password) : user.password == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
     }
 }
--- src/main/java/com/webflow/objects/UserService.java
+++ src/main/java/com/webflow/objects/UserService.java
@@ -2,17 +2,34 @@ package com.webflow.objects;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class UserService {
 
-    public String checkUser(User user) {
+    private List<User> userList = new ArrayList<>();
 
-        if (user.getName() != null && user.getName().equals("user") && user.getPassword()
-                != null && user.getPassword().equals("pass")) {
-            return "success";
-        } else {
-            return "failed";
+    public UserService() {
+        userList.add(new User("user", "pass"));
+    }
+
+    public Boolean userExist(User user) {
+
+            if (userList.contains(user)) {
+                return true;
+            }
+        return false;
+    }
+
+    public String createUser(User user){
+        for (User userExist : userList) {
+            if (userExist.getName().equals(user.getName())) {
+                return "exist";
+            }
         }
-
+        userList.add(user);
+        return "success";
     }
 }
+
